d3965114d128957a1d50b36511b9865bd092fdef
RetroNietzsche <willrazorface90s@gmail.com>
2019-11-28 11:14:14 -0300

    Restruturing

--- Windows/Desinfect.py
+++ Windows/Desinfect.py
@@ -1,13 +1,26 @@
 from Decrypter import Decrypt
 from os.path import expanduser
-from os import chdir, getenv
+from os import chdir, getenv, remove
 from string import ascii_uppercase
+
+""" ABOUT VARIABLES TO BE DECLARED
+
+    drives : list of possible drives in Windows
+    img : image to be placed on the user's desktop
+    decrypto : created instance of the Decrypt class
+    desktop : path to the user's desktop (if there is)
+    documents : path to the user documents (if there is)
+    downloads : path to the user downloads (if there is)
+    appdata : path to the user's AppData
+"""
 
 drives = []
 
 for letter in list(ascii_uppercase):
     letter = letter + ':'
     drives.append(letter)
+
+# Instantiate the Derypt class
 
 decrypto = Decrypt()
 
@@ -16,13 +29,21 @@ downloads = expanduser('~/Downloads')
 documents = expanduser('~/Documents')
 appdata = getenv('APPDATA')
 
+# Removes Run's registry created to start Rain at system boot
+
 decrypto.delete_registry(r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run', 'Rain')
+
+# Decrypt user's desktop, documents, downloads and restore the desktop background
 
 decrypto.decrypt_directory(desktop)
 decrypto.decrypt_directry(downloads)
 decrypto.decrypt_directry(documents)
 
 decrypto.restore_background(appdata)
+
+""" This structure is for decrypting each storage device connected to the machine 
+    (USB sticks, external hard drives and hard drives (external or not) )
+"""
 
 for drive in drives:
     try:
